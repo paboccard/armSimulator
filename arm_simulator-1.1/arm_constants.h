6c206473a68065f4596febe78bf8cadf5eacf2dd
boccardp <boccardp@F201-13.e-im2ag.ujf-grenoble.fr>
2015-01-06 16:40:35 +0100

    modifié:         arm_constants.h

--- arm_simulator-1.1/arm_constants.h
+++ arm_simulator-1.1/arm_constants.h
@@ -72,6 +72,24 @@ Contact: Guillaume.Huard@imag.fr
 #define BIC 14
 #define MVN 15
 
+/* conditions instructions */
+#define EQ 0
+#define NE 1
+#define CS 2
+#define CC 3
+#define MI 4
+#define PL 5
+#define VS 6
+#define VC 7
+#define HI 8
+#define LS 9
+#define GE 10
+#define LT 11
+#define GT 12
+#define LE 13
+#define AL 14
+#define UNPREDICTABLE 15
+
 /* Bit mask constants for msr */
 /* We simulate architecture v5T */
 #define UnallocMask 0x0FFFFF00
