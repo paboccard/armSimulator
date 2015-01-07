52b61dbe7d230288c35626c53ce2a43986176bce
boccardp <boccardp@f214-13.e-im2ag.ujf-grenoble.fr>
2015-01-07 15:57:17 +0100

    modifié:         arm_data_processing.c

--- arm_simulator-1.1/arm_data_processing.c
+++ arm_simulator-1.1/arm_data_processing.c
@@ -36,22 +36,19 @@ int shift_lsl(int8_t rs,int32_t val_rm, int8_t shift_imm, int8_t shift_val_imm, 
   else{ // si on a un registre pour le shift
     *cpsr = rs >= 32 ? ( rs == 32 ? get_bit(val_rm,0) : 0 ) : get_bit(val_rm,32-shift_imm);
     return rs < 32 ? val_rm << rs : 0;
-  }
-      
-      
-    
+  }  
 }
 
-  int shift_lsr(int8_t rs,int32_t val_rm, int8_t shift_imm, int8_t shift_val_imm, int *cpsr){
-    *cpsr = get_bit(val_rm,shift_imm-1) ? set_bit(*cpsr,C) : clr_bit(*cpsr,C);
-    return val_rm >> shift_imm;
+int shift_lsr(int8_t rs,int32_t val_rm, int8_t shift_imm, int8_t shift_val_imm, int *cpsr){
+  *cpsr = get_bit(val_rm,shift_imm-1) ? set_bit(*cpsr,C) : clr_bit(*cpsr,C);
+  return val_rm >> shift_imm;
+}
+ else{ // si on a un registre pour le shift
+   *cpsr = rs >= 32 ? ( rs == 32 ? get_bit(val_rm,0) : 0 ) : get_bit(val_rm,shift_imm-1);
+   return rs < 32 ? val_rm >> rs : 0;
   }
-  else{ // si on a un registre pour le shift
-    *cpsr = rs >= 32 ? ( rs == 32 ? get_bit(val_rm,0) : 0 ) : get_bit(val_rm,shift_imm-1);
-    return rs < 32 ? val_rm >> rs : 0;
-  }
-  }
-  
+}
+
   int shift_asr(int8_t rs,int32_t val_rm, int8_t shift_imm, int8_t shift_val_imm, int *cpsr){
   }
   
