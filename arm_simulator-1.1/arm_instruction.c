9a3d03e1ef093010d982fed6aabddf843ecaf209
boccardp <boccardp@F201-09.e-im2ag.ujf-grenoble.fr>
2015-01-09 13:31:28 +0100

    modifié:         arm_instruction.c

--- arm_simulator-1.1/arm_instruction.c
+++ arm_simulator-1.1/arm_instruction.c
@@ -668,7 +668,6 @@ int arm_op_ldr(arm_core p, uint32_t instr){
   uint32_t y, val_rd;
  
   rd = get_bits(instr,15,12);
-  //val_rd = arm_read_register(p,rd);
 
   y = arm_load_store(p,instr); //y = adresse de la valeur a load
   
@@ -687,11 +686,11 @@ int arm_op_ldr(arm_core p, uint32_t instr){
 }
 
 int arm_op_str(arm_core p, uint32_t instr){
-   uint8_t rd;
+  uint8_t rd;
   uint32_t y, val_rd;
  
   rd = get_bits(instr,15,12);
-  //val_rd = arm_read_register(p,rd);
+  val_rd = arm_read_register(p,rd);
 
   y = arm_load_store(p,instr); //y = adresse de la valeur a load
   
@@ -723,7 +722,7 @@ int arm_op_strb(arm_core p, uint32_t instr){
   uint32_t y, val_rd;
  
   rd = get_bits(instr,15,12);
-  //val_rd = arm_read_register(p,rd);
+  val_rd = arm_read_register(p,rd);
 
   y = arm_load_store(p,instr); //y = adresse de la valeur a load
   
@@ -755,7 +754,7 @@ int arm_op_strh(arm_core p, uint32_t instr){
   uint32_t y, val_rd;
  
   rd = get_bits(instr,15,12);
-  //val_rd = arm_read_register(p,rd);
+  val_rd = arm_read_register(p,rd);
 
   y = arm_load_store(p,instr); //y = adresse de la valeur a load
   
