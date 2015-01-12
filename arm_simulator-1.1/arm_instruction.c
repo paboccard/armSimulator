b07bd485e468bac2bd19a9fa0445e85935be0e51
boccardp <boccardp@F201-13.e-im2ag.ujf-grenoble.fr>
2015-01-12 11:21:38 +0100

    modifié:         arm_branch_other.c 	modifié:         arm_instruction.c

--- arm_simulator-1.1/arm_instruction.c
+++ arm_simulator-1.1/arm_instruction.c
@@ -664,6 +664,8 @@ int arm_op_mvn(arm_core p, uint32_t instr, int32_t *cpsr){
 
 /****************** LOAD / STORE ******************/
 int arm_op_ldr(arm_core p, uint32_t instr){
+  /*Attention aux acces non-aligne*/
+
   uint8_t rd;
   int cpsr;
   uint32_t y, val_rd;
@@ -768,25 +770,6 @@ int arm_op_strh(arm_core p, uint32_t instr){
   }
   return 0;
 }
-
-/*uint32_t start_address(arm_core p, uint32_t instr){
-  uint8_t i=0;
-  while (i<16 && (get_bit(instr,i)==0)){
-    i++;
-  }
-  return arm_read_register(p,i);
-}
-
-uint32_t end_address(arm_core p, uint32_t instr){
-  uint8_t i=0;
-  int end_address = 0;
-  while (i<16) {
-    if ((get_bit(instr,i)==0))
-      end_address++;
-    i++;
-  }
-  return arm_read_register(p,end_address);
-}*/
  
 int arm_op_ldm1(arm_core p, uint32_t instr){
   int i;
@@ -850,11 +833,6 @@ int arm_op_stm1(arm_core p, uint32_t instr){
 
 
 /****************** B / BL ******************/
-int arm_op_bl(arm_core p, uint32_t instr){
-  if (get_bit(instr,25)){
-  }
-  return 0;
-}
 
 int test_cond(uint8_t cond, arm_core p){
   int z,n,c,v;
@@ -1087,7 +1065,7 @@ static int arm_execute_instruction(arm_core p) {
 	  }
 	}
 	else {
-	  res = arm_op_bl(p,instr);
+	  res = arm_branch(p,instr);
 	  arm_write_cpsr(p,cpsr);
 	  return res;
 	}
