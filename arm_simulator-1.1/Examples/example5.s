d5c52eeee6b36e0138a9db69eb2d64e843f62078
boccardp <boccardp@F201-13.e-im2ag.ujf-grenoble.fr>
2015-01-13 15:14:40 +0100

    nouveau fichier: Examples/example5.s

---
+++ arm_simulator-1.1/Examples/example5.s
@@ -0,0 +1,12 @@
+.global main
+.text
+main:
+
+@@@@@@@@@@@@@@@@@@ Test MVN @@@@@@@@@@@@@@@@@@
+
+	MOV 	r1, #15			@ r0 = 1111
+	MOV 	r2, #0x9		@ r1 = 1001
+	MVNS 	r3, #15			@ r3 = FFFFFFF0 -> N = 1
+	MVNS 	r5, #0			@ r5 = 0 -> Z = 1 
+
+
