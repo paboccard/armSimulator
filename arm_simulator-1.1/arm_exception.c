a26a7a1140d2958515261695be7c92d9d2a6bc14
boccardp <boccardp@F201-13.e-im2ag.ujf-grenoble.fr>
2015-01-13 10:15:02 +0100

    modifié:         arm_exception.c

--- arm_simulator-1.1/arm_exception.c
+++ arm_simulator-1.1/arm_exception.c
@@ -33,13 +33,12 @@
 
 #define Exception_bit_9 (CP15_reg1_EEbit << 9)
 
-void global(arm_core p,int exception, int mode){
-    printf("Exception\n");//TODELETE
-    printf("mode : %d\n",mode);//TODELETE
+void global(arm_core p,int exception, int mode, int normal_address){
+    printf("******** Exception : %x ********\n",mode);//TODELETE
 
     uint32_t old_cpsr; 
     old_cpsr = arm_read_cpsr(p);
-
+    printf("ancien CPSR : %x\n",old_cpsr);//TODELETE
     if (mode == IRQ || mode == FIQ)
 	arm_write_register(p,14,arm_read_register(p,15)+4);
     else if (mode == ABT)
@@ -49,8 +48,9 @@ void global(arm_core p,int exception, int mode){
     
     arm_write_spsr(p,old_cpsr);
     
-    old_cpsr &= 0xFFFFFFE0 | mode;
-
+    old_cpsr = (old_cpsr & 0xFFFFFFE0) | mode;
+    printf("ancien CPSR avec le mode: %x\n",old_cpsr);//TODELETE
+    
     old_cpsr &= ~(1<<5);
     if (exception == RESET || exception == FAST_INTERRUPT)
 	old_cpsr |= 1<<6;
@@ -59,17 +59,13 @@ void global(arm_core p,int exception, int mode){
 
     if (exception == UNDEFINED_INSTRUCTION || exception == SOFTWARE_INTERRUPT)
 	old_cpsr |= 1<<8;
-
+    printf("nouveau CPSR : %x\n",old_cpsr);//TODELETE
     arm_write_cpsr(p, old_cpsr | Exception_bit_9);
-    arm_write_usr_register(p, 15, 4);
+    arm_write_usr_register(p, 15, normal_address);
 }
 
-void reset(arm_core p){
-    global(p,RESET,SVC);
-}
-
-void undefined_instruction(arm_core p){
-    /* VERSION EN CAS DE BUG
+/*void undefined_instruction(arm_core p){
+       VERSION EN CAS DE BUG
        uint32_t old_cpsr; 
        old_cpsr = arm_read_cpsr(p);
 
@@ -81,56 +77,34 @@ void undefined_instruction(arm_core p){
        old_cpsr |= 1<<7;
        arm_write_cpsr(p, old_cpsr | Exception_bit_9);
        arm_write_usr_register(p, 15, 4);
-    */
-    global(p,UNDEFINED_INSTRUCTION,UND);
-}
+    
 
-void software_interrupt(arm_core p){
-    global(p,SOFTWARE_INTERRUPT,SVC);
-}
-
-void prefetch_abort(arm_core p){
-    global(p,PREFETCH_ABORT,ABT);
-}
-
-void data_abort(arm_core p){
-    global(p,DATA_ABORT,ABT);
-}
-
-void interrupt(arm_core p){
-    global(p,INTERRUPT,IRQ);
-}
-
-void fast_interrupt(arm_core p){
-    global(p,FAST_INTERRUPT,FIQ);
-}
-
-
+}*/
 
 void arm_exception(arm_core p, unsigned char exception) {
     /* We only support RESET initially */
     /* Semantics of reset interrupt (ARM manual A2-18) */
     switch(exception){
     case(RESET):
-	reset(p);
+	global(p,RESET,SVC,0);
 	break;
     case(UNDEFINED_INSTRUCTION):
-	undefined_instruction(p);
+	global(p,UNDEFINED_INSTRUCTION,UND,4);
 	break;
     case(SOFTWARE_INTERRUPT):
-	software_interrupt(p);
+	global(p,SOFTWARE_INTERRUPT,SVC,8);
 	break;
     case(PREFETCH_ABORT):
-	prefetch_abort(p);
+	global(p,PREFETCH_ABORT,ABT,12);
 	break;
     case(DATA_ABORT):
-	data_abort(p);
+	global(p,DATA_ABORT,ABT,16);
 	break;
     case(INTERRUPT):
-	interrupt(p);
+	global(p,INTERRUPT,IRQ,24);
 	break;
     case(FAST_INTERRUPT):
-	fast_interrupt(p);
+	global(p,FAST_INTERRUPT,FIQ,28);
 	break;
     }
 }
